32bf69c2bce96e48c84c78245ae2e61bccf5a3f4
devendras1990 <kdevbisht1990@gmail.com>
2024-11-18 16:09:29 +0530

    updayte

--- src/components/FlipTitle.tsx
+++ src/components/FlipTitle.tsx
@@ -6,7 +6,7 @@ export default function FlipTitle() {
 
   return (
       <h1 className="text-3xl md:text-6xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-950 to-neutral-400 dark:from-neutral-50 dark:to-neutral-400  bg-opacity-50">
-      Hi, I&apos;m Devendra:Visible ui ux Designer<br />
+      Hi, I&apos;m Devendra:Visible Designer<br />
         <FlipWords words={words} /> <br />
         That Stand Out
       </h1> 
