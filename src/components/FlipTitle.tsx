da2987b8a3df6c8121a067f58b8edd8d4a5d0e19
devendras1990 <kdevbisht1990@gmail.com>
2024-11-18 16:12:55 +0530

    update

--- src/components/FlipTitle.tsx
+++ src/components/FlipTitle.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FlipWords } from "./ui/flip-words";
 
 export default function FlipTitle() {
-  const words = ["Web Designs", "Frontend Projects", "Digital Experiences", "Interfaces"];
+  const words = ["Wireframing", "Prototyping", "User research", "Graphic design", "User centred design"];
 
   return (
       <h1 className="text-3xl md:text-6xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-950 to-neutral-400 dark:from-neutral-50 dark:to-neutral-400  bg-opacity-50">
