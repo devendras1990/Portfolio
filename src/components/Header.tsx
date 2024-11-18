4b50bab6bbe6c829fc6238284509f73a29835441
devendras1990 <kdevbisht1990@gmail.com>
2024-11-18 17:00:18 +0530

    header remove

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 'use client';
 import React from 'react'
 import Navbar from './Navbar'
-import { usePathname } from 'next/navigation'
+// import { usePathname } from 'next/navigation'
 const Header = () => {
-    const pathname = usePathname()
+    // const pathname = usePathname()
   return (
     <>
-    {pathname === '/' && <Navbar className="top-5 left-3 right-3" />}
+    {/* {pathname === '/' && <Navbar className="top-5 left-3 right-3" />} */}
     </>
   )
 }
