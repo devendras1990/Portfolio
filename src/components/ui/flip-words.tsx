6b3b8309e985b6c88584d134156eb726c75ac1dd
devendras1990 <kdevbisht1990@gmail.com>
2024-11-18 17:07:31 +0530

    color update

--- src/components/ui/flip-words.tsx
+++ src/components/ui/flip-words.tsx
@@ -58,7 +58,7 @@ export const FlipWords = ({
           position: "absolute",
         }}
         className={cn(
-          "z-10 inline-block relative text-left text-[#a3e4d7] dark:text-neutral-100 px-2",
+          "z-10 inline-block relative text-left text-[#a3e4d7] dark:text-[#a3e4d7] px-2",
           className
         )}
         key={currentWord}
